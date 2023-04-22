fe529c046b5ffd284efe32540c168aba49fff76a
busungg <busungg@naver.com>
2023-04-22 11:25:16 +0900

    spark s3 구축

---
+++ dockerfile/master/etc.profile.d/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$JAVA_HOME" ]; then
+  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.362.b09-2.el8_7.x86_64
+  export PATH="$PATH:$JAVA_HOME:$JAVA_HOME/bin"
+fi
+
+if [ -z "$SPARK_HOME" ]; then
+  export SPARK_HOME=/usr/local/spark
+  export PATH="$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin"
+fi
